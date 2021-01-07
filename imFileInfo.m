020b7b2cc3b41f4d7b022f7365a10fa9f325ab04
davinchi84 <sungu09@gmail.com>
2021-01-07 10:31:28 -0600

    Still updating script for data processing

--- imFileInfo.m
+++ imFileInfo.m
@@ -1,94 +1,124 @@
+%% 0 min, ICP zone 
 texRed_0min.name = 'texRed_0min';
-texRed_0min.path = [pwd '\' '0min'];
-texRed_0min.fileName = 'lysis&assay_0minc3.tif';
+texRed_0min.path = [pwd '\' '0min' '\' 'ICPOff'];
+texRed_0min.fileName = 'initial_icp_zonec3.tif';
 texRed_0min.path_n_filename = [texRed_0min.path '\' texRed_0min.fileName];
 texRed_0min.IM = [];
 
-%%
 GFP_0min.name = 'GFP_0min';
-GFP_0min.path = [pwd '\' '0min'];
-GFP_0min.fileName = 'lysis&assay_0minc2.tif';
+GFP_0min.path = [pwd '\' '0min' '\' 'ICPOff'];
+GFP_0min.fileName = 'initial_icp_zonec2.tif';
 GFP_0min.path_n_filename = [GFP_0min.path '\' GFP_0min.fileName];
 GFP_0min.IM = [];
 
-%%
-texRed_5min.name = 'texRed_5min';
-texRed_5min.path = [pwd '\' '5min'];
-texRed_5min.fileName = 'lysis&assay_5min_10c3.tif';
-texRed_5min.path_n_filename = [texRed_5min.path '\' texRed_5min.fileName];
-texRed_5min.IM = [];
+%% 0 min, no ICP zone
+texRed_noICP_0min.name = 'texRed_noICP_0min';
+texRed_noICP_0min.path = [pwd '\' '0min' '\' 'ICPOff'];
+texRed_noICP_0min.fileName = 'upstream_initial_non_icp_zonec3.tif';
+texRed_noICP_0min.path_n_filename = [texRed_0min.path '\' texRed_0min.fileName];
+texRed_noICP_0min.IM = [];
 
-%%
+GFP_noICP_0min.name = 'GFP_noICP_0min';
+GFP_noICP_0min.path = [pwd '\' '0min' '\' 'ICPOff'];
+GFP_noICP_0min.fileName = 'upstream_initial_non_icp_zonec2.tif';
+GFP_noICP_0min.path_n_filename = [GFP_0min.path '\' GFP_0min.fileName];
+GFP_noICP_0min.IM = [];
+
+%% 5 min, ICP zone 
+texRed_ICP_5min.name = 'texRed_ICP_5min';
+texRed_ICP_5min.path = [pwd '\' '5min' '\' 'ICP'];
+texRed_ICP_5min.fileName = '4v_icp_5_minc3.tif';
+texRed_ICP_5min.path_n_filename = [texRed_ICP_5min.path '\' texRed_ICP_5min.fileName];
+texRed_ICP_5min.IM = [];
+
 GFP_ICP_5min.name = 'GFP_ICP_5min';
-GFP_ICP_5min.path = [pwd '\' '5min'];
-GFP_ICP_5min.fileName = 'lysis&assay_5min_10c2.tif';
+GFP_ICP_5min.path = [pwd '\' '5min' '\' 'ICP'];
+GFP_ICP_5min.fileName = '4v_icp_5_minc2.tif';
 GFP_ICP_5min.path_n_filename = [GFP_ICP_5min.path '\' GFP_ICP_5min.fileName];
 GFP_ICP_5min.IM = [];
 
-%%
-texRed_10min.name = 'texRed_10min';
-texRed_10min.path = [pwd '\' '10min'];
-texRed_10min.fileName = 'lysis&assay_10minc3.tif';
-texRed_10min.path_n_filename = [texRed_10min.path '\' texRed_10min.fileName];
-texRed_10min.IM = [];
+%% 5 min, out of ICP zone 
+texRed_noICP_5min.name = 'texRed_noICP_5min';
+texRed_noICP_5min.path = [pwd '\' '5min' '\' 'noICP'];
+texRed_noICP_5min.fileName = '4v_noicp_5 minc3.tif';
+texRed_noICP_5min.path_n_filename = [texRed_noICP_5min.path '\' texRed_noICP_5min.fileName];
+texRed_noICP_5min.IM = [];
+
+GFP_noICP_5min.name = 'GFP_noICP_5min';
+GFP_noICP_5min.path = [pwd '\' '5min' '\' 'noICP'];
+GFP_noICP_5min.fileName = '4v_noicp_5 minc2.tif';
+GFP_noICP_5min.path_n_filename = [GFP_noICP_5min.path '\' GFP_noICP_5min.fileName];
+GFP_noICP_5min.IM = [];
 
 %%
-GFP_ICP_10min.name = 'GFP_ICP_10min';
-GFP_ICP_10min.path = [pwd '\' '10min'];
-GFP_ICP_10min.fileName = 'lysis&assay_10minc2.tif';
-GFP_ICP_10min.path_n_filename = [GFP_ICP_10min.path '\' GFP_ICP_10min.fileName];
-GFP_ICP_10min.IM = [];
+dataInfo = {texRed_0min, GFP_0min, texRed_noICP_0min, GFP_noICP_0min, ...
+    texRed_ICP_5min, GFP_ICP_5min, texRed_noICP_5min, GFP_noICP_5min};
+
+% %%
+% texRed_10min.name = 'texRed_10min';
+% texRed_10min.path = [pwd '\' '10min'];
+% texRed_10min.fileName = 'lysis&assay_10minc3.tif';
+% texRed_10min.path_n_filename = [texRed_10min.path '\' texRed_10min.fileName];
+% texRed_10min.IM = [];
+% 
+% %%
+% GFP_ICP_10min.name = 'GFP_ICP_10min';
+% GFP_ICP_10min.path = [pwd '\' '10min'];
+% GFP_ICP_10min.fileName = 'lysis&assay_10minc2.tif';
+% GFP_ICP_10min.path_n_filename = [GFP_ICP_10min.path '\' GFP_ICP_10min.fileName];
+% GFP_ICP_10min.IM = [];
+% 
+% %%
+% texRed_15min.name = 'texRed_15min';
+% texRed_15min.path = [pwd '\' '15min'];
+% texRed_15min.fileName = 'lysis&assay_15min001c3.tif';
+% texRed_15min.path_n_filename = [texRed_15min.path '\' texRed_15min.fileName];
+% texRed_15min.IM = [];
+% 
+% %%
+% GFP_ICP_15min.name = 'GFP_ICP_15min';
+% GFP_ICP_15min.path = [pwd '\' '15min'];
+% GFP_ICP_15min.fileName = 'lysis&assay_15min001c2.tif';
+% GFP_ICP_15min.path_n_filename = [GFP_ICP_15min.path '\' GFP_ICP_15min.fileName];
+% GFP_ICP_15min.IM = [];
+% 
+% %%
+% texRed_final.name = 'texRed_final';
+% texRed_final.path = [pwd '\' '20min'];
+% texRed_final.fileName = 'lysis&assay_final_wo_icpc3.tif';
+% texRed_final.path_n_filename = [texRed_final.path '\' texRed_final.fileName];
+% texRed_final.IM = [];
+% 
+% %%
+% GFP_final.name = 'GFP_final';
+% GFP_final.path = [pwd '\' '20min'];
+% GFP_final.fileName = 'lysis&assay_final_wo_icpc2.tif';
+% GFP_final.path_n_filename = [GFP_final.path '\' GFP_final.fileName];
+% GFP_final.IM = [];
+% 
+% %%
+% GFP_final_ICP.name = 'GFP_final_ICP';
+% GFP_final_ICP.path = [pwd '\' '20min'];
+% GFP_final_ICP.fileName = 'lysis&assay_20min002c2.tif';
+% GFP_final_ICP.path_n_filename = [GFP_final_ICP.path '\' GFP_final_ICP.fileName];
+% GFP_final_ICP.IM = [];
+% 
+% %%
+% texRed_noICP_final.name = 'texRed_noICP_final';
+% texRed_noICP_final.path = [pwd '\' 'noICP_final'];
+% texRed_noICP_final.fileName = 'lysis&assay_nonicpzonec3.tif';
+% texRed_noICP_final.path_n_filename = [texRed_noICP_final.path '\' texRed_noICP_final.fileName];
+% texRed_noICP_final.IM = [];
+% 
+% %%
+% GFP_noICP_final.name = 'GFP_noICP_final';
+% GFP_noICP_final.path = [pwd '\' 'noICP_final'];
+% GFP_noICP_final.fileName = 'lysis&assay_nonicpzonec2.tif';
+% GFP_noICP_final.path_n_filename = [GFP_noICP_final.path '\' GFP_noICP_final.fileName];
+% GFP_noICP_final.IM = [];
 
 %%
-texRed_15min.name = 'texRed_15min';
-texRed_15min.path = [pwd '\' '15min'];
-texRed_15min.fileName = 'lysis&assay_15min001c3.tif';
-texRed_15min.path_n_filename = [texRed_15min.path '\' texRed_15min.fileName];
-texRed_15min.IM = [];
 
-%%
-GFP_ICP_15min.name = 'GFP_ICP_15min';
-GFP_ICP_15min.path = [pwd '\' '15min'];
-GFP_ICP_15min.fileName = 'lysis&assay_15min001c2.tif';
-GFP_ICP_15min.path_n_filename = [GFP_ICP_15min.path '\' GFP_ICP_15min.fileName];
-GFP_ICP_15min.IM = [];
-
-%%
-texRed_final.name = 'texRed_final';
-texRed_final.path = [pwd '\' '20min'];
-texRed_final.fileName = 'lysis&assay_final_wo_icpc3.tif';
-texRed_final.path_n_filename = [texRed_final.path '\' texRed_final.fileName];
-texRed_final.IM = [];
-
-%%
-GFP_final.name = 'GFP_final';
-GFP_final.path = [pwd '\' '20min'];
-GFP_final.fileName = 'lysis&assay_final_wo_icpc2.tif';
-GFP_final.path_n_filename = [GFP_final.path '\' GFP_final.fileName];
-GFP_final.IM = [];
-
-%%
-GFP_final_ICP.name = 'GFP_final_ICP';
-GFP_final_ICP.path = [pwd '\' '20min'];
-GFP_final_ICP.fileName = 'lysis&assay_20min002c2.tif';
-GFP_final_ICP.path_n_filename = [GFP_final_ICP.path '\' GFP_final_ICP.fileName];
-GFP_final_ICP.IM = [];
-
-%%
-texRed_noICP_final.name = 'texRed_noICP_final';
-texRed_noICP_final.path = [pwd '\' 'noICP_final'];
-texRed_noICP_final.fileName = 'lysis&assay_nonicpzonec3.tif';
-texRed_noICP_final.path_n_filename = [texRed_noICP_final.path '\' texRed_noICP_final.fileName];
-texRed_noICP_final.IM = [];
-
-%%
-GFP_noICP_final.name = 'GFP_noICP_final';
-GFP_noICP_final.path = [pwd '\' 'noICP_final'];
-GFP_noICP_final.fileName = 'lysis&assay_nonicpzonec2.tif';
-GFP_noICP_final.path_n_filename = [GFP_noICP_final.path '\' GFP_noICP_final.fileName];
-GFP_noICP_final.IM = [];
-
-%%
-dataInfo = {texRed_0min, texRed_5min, texRed_10min, texRed_15min, texRed_final,...
-    GFP_0min, GFP_ICP_5min, GFP_ICP_10min, GFP_ICP_15min, GFP_final, GFP_final_ICP,...
-    GFP_noICP_final, texRed_noICP_final};
+% dataInfo = {texRed_0min, texRed_5min, texRed_10min, texRed_15min, texRed_final,...
+%     GFP_0min, GFP_ICP_5min, GFP_ICP_10min, GFP_ICP_15min, GFP_final, GFP_final_ICP,...
+%     GFP_noICP_final, texRed_noICP_final};
--- process.m
+++ process.m
@@ -7,73 +7,87 @@ lineWidth = 2;
 markerSize = 12;
 
 tic
-%% 0minializing 
+%% class initializing  
 icpImProc = ICPIMPROC;
 
-%% GFP masking and drop size for analysis 
+%% 0 min
+% in ICP zone
 icpImProc.masking('GFP_0min','texRed_0min');
-icpImProc.masking('GFP_final','texRed_final');
 dropSize_0min = icpImProc.imIntSum('texRed_0min','bw');
-dropSize_final = icpImProc.imIntSum('texRed_final','bw');
 
-%% GFP average intensity, 0min
-intSum_GFP_0min = icpImProc.imIntSum('GFP_0min','bgOut');
-avgInt_GFP_0min = intSum_GFP_0min/dropSize_0min;
+% out of ICP zone
+icpImProc.masking('GFP_noICP_0min','texRed_noICP_0min');
+dropSize_noICP_0min = icpImProc.imIntSum('texRed_noICP_0min','bw');
 
-%% GFP increased intensity, ICP off 
-icpImProc.masking('GFP_final_ICP','texRed_final');
-GFP_diff_final = icpImProc.imSubtract('GFP_final','bgOut','GFP_0min','bgOut');
-GFP_diff_final_avg = sum(GFP_diff_final(:))/dropSize_final;
-GFP_EF_mean = 1+GFP_diff_final_avg/avgInt_GFP_0min;
+%% 5 min 
+% in ICP zone
+icpImProc.masking('GFP_ICP_5min','texRed_ICP_5min');
+dropSize_0min = icpImProc.imIntSum('texRed_ICP_5min','bw');
+montage({icpImProc.getIm('GFP_ICP_5min','gr')*50,icpImProc.getIm('texRed_ICP_5min','bw')});
 
-%% max GFP EF, ICP on
-% 5 min
-icpImProc.masking('GFP_ICP_5min','texRed_5min');
-GFP_ICP_diff_5min = icpImProc.imSubtract('GFP_ICP_5min','bgOut','GFP_0min','bgOut');
-GFP_ICP_EF_5min_max = 1+double(max(max(GFP_ICP_diff_5min)))/avgInt_GFP_0min;
+a = 1
+% icpImProc.masking('GFP_final','texRed_final');
 
-% 10 min
-icpImProc.masking('GFP_ICP_10min','texRed_10min');
-GFP_ICP_diff_10min = icpImProc.imSubtract('GFP_ICP_10min','bgOut','GFP_0min','bgOut');
-GFP_ICP_EF_10min_max = 1+double(max(max(GFP_ICP_diff_10min)))/avgInt_GFP_0min;
-
-% 15 min
-icpImProc.masking('GFP_ICP_15min','texRed_15min');
-GFP_ICP_diff_15min = icpImProc.imSubtract('GFP_ICP_15min','bgOut','GFP_0min','bgOut');
-GFP_ICP_EF_15min_max = 1+double(max(max(GFP_ICP_diff_15min)))/avgInt_GFP_0min;
-
-GFP_ICP_diff_final = icpImProc.imSubtract('GFP_final_ICP','bgOut','GFP_0min','bgOut');
-GFP_ICP_EF_final_max = 1+double(max(max(GFP_ICP_diff_final)))/avgInt_GFP_0min; 
-
-%% out of ICP zone droplet 
-%final
-icpImProc.masking('GFP_noICP_final','texRed_noICP_final');
-dropSize_noICP_final = icpImProc.imIntSum('texRed_noICP_final','bw');
-intSum_GFP_noICP_final = icpImProc.imIntSum('GFP_noICP_final','bgOut');
-avgInt_GFP_noICP_final = intSum_GFP_noICP_final/dropSize_noICP_final;
-EF_GFP_noICP_final = avgInt_GFP_noICP_final/avgInt_GFP_0min;
-
-%% plots
-%EF
-t = [0 5 10 15 20];
-EF_ICP = [1 GFP_ICP_EF_5min_max GFP_ICP_EF_10min_max GFP_ICP_EF_15min_max ...
-    GFP_ICP_EF_final_max];
-
-plot(t,EF_ICP,'-o','LineWidth',lineWidth,'MarkerSize',markerSize), hold on
-plot([0 20],[GFP_EF_mean GFP_EF_mean],'-x','LineWidth',lineWidth,...
-    'MarkerSize',markerSize)
-plot([0 20],[EF_GFP_noICP_final EF_GFP_noICP_final],'-*',...
-    'LineWidth',lineWidth,'MarkerSize',markerSize)
-
-%accessaries 
-axis([-1 21 -inf inf])
-xlabel('Time (min)','Interpreter','latex')
-ylabel('Enrichment Factor','Interpreter','latex')
-legend('Max EF w/ ICP', 'Droplet EF w/o ICP', 'EF ICP never experienced');
-
-%save
-print('EF','-dpng')
-
-%%
-toc
+% dropSize_final = icpImProc.imIntSum('texRed_final','bw');
+% 
+% %% GFP average intensity, 0min
+% intSum_GFP_0min = icpImProc.imIntSum('GFP_0min','bgOut');
+% avgInt_GFP_0min = intSum_GFP_0min/dropSize_0min;
+% 
+% %% GFP increased intensity, ICP off 
+% icpImProc.masking('GFP_final_ICP','texRed_final');
+% GFP_diff_final = icpImProc.imSubtract('GFP_final','bgOut','GFP_0min','bgOut');
+% GFP_diff_final_avg = sum(GFP_diff_final(:))/dropSize_final;
+% GFP_EF_mean = 1+GFP_diff_final_avg/avgInt_GFP_0min;
+% 
+% %% max GFP EF, ICP on
+% % 5 min
+% icpImProc.masking('GFP_ICP_5min','texRed_5min');
+% GFP_ICP_diff_5min = icpImProc.imSubtract('GFP_ICP_5min','bgOut','GFP_0min','bgOut');
+% GFP_ICP_EF_5min_max = 1+double(max(max(GFP_ICP_diff_5min)))/avgInt_GFP_0min;
+% 
+% % 10 min
+% icpImProc.masking('GFP_ICP_10min','texRed_10min');
+% GFP_ICP_diff_10min = icpImProc.imSubtract('GFP_ICP_10min','bgOut','GFP_0min','bgOut');
+% GFP_ICP_EF_10min_max = 1+double(max(max(GFP_ICP_diff_10min)))/avgInt_GFP_0min;
+% 
+% % 15 min
+% icpImProc.masking('GFP_ICP_15min','texRed_15min');
+% GFP_ICP_diff_15min = icpImProc.imSubtract('GFP_ICP_15min','bgOut','GFP_0min','bgOut');
+% GFP_ICP_EF_15min_max = 1+double(max(max(GFP_ICP_diff_15min)))/avgInt_GFP_0min;
+% 
+% GFP_ICP_diff_final = icpImProc.imSubtract('GFP_final_ICP','bgOut','GFP_0min','bgOut');
+% GFP_ICP_EF_final_max = 1+double(max(max(GFP_ICP_diff_final)))/avgInt_GFP_0min; 
+% 
+% %% out of ICP zone droplet 
+% %final
+% icpImProc.masking('GFP_noICP_final','texRed_noICP_final');
+% dropSize_noICP_final = icpImProc.imIntSum('texRed_noICP_final','bw');
+% intSum_GFP_noICP_final = icpImProc.imIntSum('GFP_noICP_final','bgOut');
+% avgInt_GFP_noICP_final = intSum_GFP_noICP_final/dropSize_noICP_final;
+% EF_GFP_noICP_final = avgInt_GFP_noICP_final/avgInt_GFP_0min;
+% 
+% %% plots
+% %EF
+% t = [0 5 10 15 20];
+% EF_ICP = [1 GFP_ICP_EF_5min_max GFP_ICP_EF_10min_max GFP_ICP_EF_15min_max ...
+%     GFP_ICP_EF_final_max];
+% 
+% plot(t,EF_ICP,'-o','LineWidth',lineWidth,'MarkerSize',markerSize), hold on
+% plot([0 20],[GFP_EF_mean GFP_EF_mean],'-x','LineWidth',lineWidth,...
+%     'MarkerSize',markerSize)
+% plot([0 20],[EF_GFP_noICP_final EF_GFP_noICP_final],'-*',...
+%     'LineWidth',lineWidth,'MarkerSize',markerSize)
+% 
+% %accessaries 
+% axis([-1 21 -inf inf])
+% xlabel('Time (min)','Interpreter','latex')
+% ylabel('Enrichment Factor','Interpreter','latex')
+% legend('Max EF w/ ICP', 'Droplet EF w/o ICP', 'EF ICP never experienced');
+% 
+% %save
+% print('EF','-dpng')
+% 
+% %%
+% toc
 
